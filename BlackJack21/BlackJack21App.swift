125b839a4814f032e0589d8e0f711f6d792a95cb
ArshdeepSB <arshdeepbadhan1@gmail.com>
2022-11-10 17:28:17 -0500

    Initial Commit

---
+++ BlackJack21/BlackJack21App.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJack21App.swift
+//  BlackJack21
+//
+//  Created by Arshdeep Badhan on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct BlackJack21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
